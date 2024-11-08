cc07287c816a06e68ca69b1f4086aa2d2aa273bc
Jean le Grand Bokassa <jean.le.grand.bokassa@gmail.com>
2024-11-08 03:20:19 +0100

    ended the TP

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+$contacts = [
+    [
+        "name" => "Jean Dupont",
+        "email" => "jean.dupont@example.com",
+        "phone" => "0102030405"
+    ],
+    [
+        "name" => "Marie Curie",
+        "email" => "marie.curie@example.com",
+        "phone" => "0607080910"
+    ],
+];
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php require_once 'data.php'; ?>
+<?php
+// initialize variables
+$name = $email = $phone = '';
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,11 +16,10 @@
 <body>
 <div class="container py-4">
     <?php require_once 'header.php'; ?>
-
     <!-- Contact Form -->
     <div class="container-fluid py-5">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-12 col-md-6">
                 <h2 class="mb-3">Add Contact</h2>
                 <form method="post">
                     <div class="mb-3">
@@ -32,7 +37,7 @@
                     <button type="submit" class="btn btn-outline-secondary btn-sm">Add Contact</button>
                 </form>
             </div>
-            <div class="col-md-6">
+            <div class="col-12 col-md-6">
                 <h2 class="mb-3">Contacts</h2>
                 <?php
                 // If server request method is POST
@@ -48,20 +53,48 @@
                         echo '<div class="alert alert-success">Contact added successfully</div>';
 
                         // Show contact details in a var_dump
-                        var_dump($name, $email, $phone);
+                        // var_dump($name, $email, $phone);
+
+                        // Add contact to contacts array $contacts
+                        $contacts[] = [
+                            'name' => $name,
+                            'email' => $email,
+                            'phone' => $phone
+                        ];
+
+                        // Show contacts in a var_dump
+                        /*echo '<pre>';
+                        var_dump($contacts);
+                        echo '</pre>';*/
+
                     } else {
                         // Display error message
                         echo '<div class="alert alert-danger">All fields are required</div>';
                     }
                 }
                 ?>
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($contacts as $contact): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($contact['name']); ?></td>
+                            <td><?= htmlspecialchars($contact['email']); ?></td>
+                            <td><?= htmlspecialchars($contact['phone']); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
             </div>
         </div>
+
     </div>
-
-    <?php require_once 'footer.php'; ?>
-
-
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossorigin="anonymous"></script>
